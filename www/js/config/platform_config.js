4f5a3aadd51d995db0e7c73623d591fe1929e22e
Tomas Diaz <tomas.diaz@xintec.cl>
2018-01-16 21:27:45 -0300

    implement onesignal

--- www/js/config/platform_config.js
+++ www/js/config/platform_config.js
@@ -16,21 +16,24 @@ PLATFORM CONFIGURATION
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(false);
 
-        // var notificationOpenedCallback = function(jsonData) {
-        //     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-        //   };
-        //
-        //   window.plugins.OneSignal.startInit("694b5cc5-8457-4196-836f-d814baf6eb00").endInit();
-        //     // .handleNotificationOpened(notificationOpenedCallback)
-        //
-        //
-        //     window.plugins.OneSignal.getIds(function (ids) {
-        //          console.log('UserID:  ' + ids.userId);
-        //          console.log('PushToken:  ' + ids.pushToken);
-        //
-        //          // $rootScope.user_id = ids.userId;
-        //          // $rootScope.pushToken = ids.pushToken;
-        //      });
+        var notificationOpenedCallback = function(jsonData) {
+            console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+          };
+
+          window.plugins.OneSignal
+            .startInit("3169e7d1-aa60-4fd6-920b-62a86a2e34c0")
+            .handleNotificationOpened(notificationOpenedCallback)
+            .endInit();
+            // .handleNotificationOpened(notificationOpenedCallback)
+
+
+            window.plugins.OneSignal.getIds(function (ids) {
+                 console.log('UserID:  ' + ids.userId);
+                 console.log('PushToken:  ' + ids.pushToken);
+
+                 // $rootScope.user_id = ids.userId;
+                 // $rootScope.pushToken = ids.pushToken;
+             });
 
       }
 
@@ -42,5 +45,5 @@ PLATFORM CONFIGURATION
     });
   })
 
-  
+
 }).call(this);
